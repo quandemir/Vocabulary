93535ee1c889e6237c44e62e1960fd23daec8817
Osman KANDEMIR <kandemirosman6925@gmail.com>
2023-08-23 14:28:43 +0300

    Add project files.

---
+++ Vocabulary/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Vocabulary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ToplamSoru = new System.Windows.Forms.Label();
+            this.Doğru = new System.Windows.Forms.Label();
+            this.Yanlış = new System.Windows.Forms.Label();
+            this.Toplam = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Soru = new System.Windows.Forms.Button();
+            this.ctarih = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(82, 126);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.CheckAnswer);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button2.Location = new System.Drawing.Point(238, 126);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 58);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.CheckAnswer);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button3.Location = new System.Drawing.Point(82, 191);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 58);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.CheckAnswer);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button4.Location = new System.Drawing.Point(238, 191);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 58);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.CheckAnswer);
+            // 
+            // ToplamSoru
+            // 
+            this.ToplamSoru.AutoSize = true;
+            this.ToplamSoru.Location = new System.Drawing.Point(20, 343);
+            this.ToplamSoru.Name = "ToplamSoru";
+            this.ToplamSoru.Size = new System.Drawing.Size(14, 16);
+            this.ToplamSoru.TabIndex = 5;
+            this.ToplamSoru.Text = "0";
+            // 
+            // Doğru
+            // 
+            this.Doğru.AutoSize = true;
+            this.Doğru.Location = new System.Drawing.Point(71, 343);
+            this.Doğru.Name = "Doğru";
+            this.Doğru.Size = new System.Drawing.Size(14, 16);
+            this.Doğru.TabIndex = 6;
+            this.Doğru.Text = "0";
+            // 
+            // Yanlış
+            // 
+            this.Yanlış.AutoSize = true;
+            this.Yanlış.Location = new System.Drawing.Point(117, 342);
+            this.Yanlış.Name = "Yanlış";
+            this.Yanlış.Size = new System.Drawing.Size(14, 16);
+            this.Yanlış.TabIndex = 7;
+            this.Yanlış.Text = "0";
+            // 
+            // Toplam
+            // 
+            this.Toplam.AutoSize = true;
+            this.Toplam.Location = new System.Drawing.Point(-1, 317);
+            this.Toplam.Name = "Toplam";
+            this.Toplam.Size = new System.Drawing.Size(54, 16);
+            this.Toplam.TabIndex = 8;
+            this.Toplam.Text = "Toplam";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Doğru";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 317);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Yanlış";
+            // 
+            // Soru
+            // 
+            this.Soru.BackColor = System.Drawing.Color.Transparent;
+            this.Soru.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Soru.FlatAppearance.BorderSize = 0;
+            this.Soru.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Soru.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Soru.Location = new System.Drawing.Point(82, 61);
+            this.Soru.Name = "Soru";
+            this.Soru.Size = new System.Drawing.Size(304, 58);
+            this.Soru.TabIndex = 11;
+            this.Soru.Text = "Soru";
+            this.Soru.UseVisualStyleBackColor = false;
+            // 
+            // ctarih
+            // 
+            this.ctarih.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ctarih.FormattingEnabled = true;
+            this.ctarih.Items.AddRange(new object[] {
+            "All"});
+            this.ctarih.Location = new System.Drawing.Point(371, 12);
+            this.ctarih.Name = "ctarih";
+            this.ctarih.Size = new System.Drawing.Size(138, 28);
+            this.ctarih.TabIndex = 12;
+            this.ctarih.SelectedIndexChanged += new System.EventHandler(this.ctarih_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(521, 374);
+            this.Controls.Add(this.ctarih);
+            this.Controls.Add(this.Soru);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Toplam);
+            this.Controls.Add(this.Yanlış);
+            this.Controls.Add(this.Doğru);
+            this.Controls.Add(this.ToplamSoru);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Vocabulary";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label ToplamSoru;
+        private System.Windows.Forms.Label Doğru;
+        private System.Windows.Forms.Label Yanlış;
+        private System.Windows.Forms.Label Toplam;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Soru;
+        private System.Windows.Forms.ComboBox ctarih;
+    }
+}
+
---
+++ Vocabulary/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vocabulary
+{
+    public partial class Form1 : Form
+    {
+        private List<string> englishWords = new List<string>();
+        private List<string> turkishWords = new List<string>();
+        private List<string> dates = new List<string>();
+        private Random random = new Random();
+        private Button[] answerButtons;
+        private string currentEnglishWord;
+        private string correctTurkishTranslation;
+        int doğru = 0;
+        int yanlış = 0;
+        int ToplamSayı = -1;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeWordLists();
+            ctarih.DataSource = dates;
+            answerButtons = new Button[] { button1, button2, button3, button4 };
+            ShowRandomWord();
+        }
+        private void InitializeWordLists()
+        {
+            //Burada englishWords ve turkishWords listelerini doldurun.
+            OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\kande\OneDrive\Masaüstü\Vocabulary.xlsx; Extended Properties='Excel 12.0 xml;HDR=YES;'");
+            OleDbCommand cmd = new OleDbCommand("Select * from [Sayfa1$]", con);
+            con.Open();
+            OleDbDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string englishWord = reader["İNGİLİZCE"].ToString();
+                string turkishWord = reader["TÜRKÇE"].ToString();
+                string tarih = reader["TARİH"].ToString();
+                string[] x = tarih.Split(' ');
+                string düzgüntarih = x[0]; // İlk kelimeyi al
+
+                if (!string.IsNullOrEmpty(englishWord) && !string.IsNullOrEmpty(turkishWord))
+                {
+                    englishWords.Add(englishWord);
+                    turkishWords.Add(turkishWord);
+                    if (!dates.Contains("All"))
+                    {
+                        dates.Add("All");
+                    }
+                    if (!dates.Contains(düzgüntarih))
+                    {
+                        dates.Add(düzgüntarih);
+                    }
+                }
+            }
+
+        }
+        private void ShowRandomWord()
+        {
+
+            int randomIndex = random.Next(englishWords.Count);
+            currentEnglishWord = englishWords[randomIndex];
+            correctTurkishTranslation = turkishWords[randomIndex];
+
+            Soru.Text = currentEnglishWord;
+
+            // Sadece doğru çevabı bir butona yerleştirin
+            int correctButtonIndex = random.Next(answerButtons.Length);
+            answerButtons[correctButtonIndex].Text = correctTurkishTranslation;
+
+            // Diğer butonlara farklı cevapları yerleştirin
+            for (int i = 0; i < answerButtons.Length; i++)
+            {
+                if (i != correctButtonIndex)
+                {
+                    string incorrectTranslation = GetRandomIncorrectTranslation();
+                    answerButtons[i].Text = incorrectTranslation;
+                }
+            }
+            ToplamSayı++;
+            ToplamSoru.Text = ToplamSayı.ToString();
+        }
+        private string GetRandomIncorrectTranslation()
+        {
+            string randomIncorrectTranslation;
+            do
+            {
+                int randomIndex = random.Next(turkishWords.Count);
+                randomIncorrectTranslation = turkishWords[randomIndex];
+            } while (randomIncorrectTranslation == correctTurkishTranslation);
+
+            return randomIncorrectTranslation;
+        }
+
+        private async void CheckAnswer(object sender, EventArgs e)
+        {
+
+            Button clickedButton = sender as Button;
+            int correctButtonIndex = Array.IndexOf(answerButtons, Array.Find(answerButtons, btn => btn.Text == correctTurkishTranslation));
+
+            if (clickedButton.Text == correctTurkishTranslation)
+            {
+                clickedButton.BackColor = Color.Green;
+                clickedButton.FlatAppearance.BorderColor = Color.Green;
+                await Task.Delay(150);
+                doğru++;
+                Doğru.Text = doğru.ToString();
+                clickedButton.BackColor = Color.White;
+                clickedButton.FlatAppearance.BorderColor = Color.White;
+            }
+            else
+            {
+                clickedButton.BackColor = Color.Red;
+                clickedButton.FlatAppearance.BorderColor = Color.Red;
+                answerButtons[correctButtonIndex].BackColor = Color.Green;
+                answerButtons[correctButtonIndex].FlatAppearance.BorderColor = Color.Green;
+                await Task.Delay(500);
+                yanlış++;
+                Yanlış.Text = yanlış.ToString();
+                clickedButton.BackColor = Color.White;
+                clickedButton.FlatAppearance.BorderColor = Color.White;
+                answerButtons[correctButtonIndex].BackColor = Color.White;
+                answerButtons[correctButtonIndex].FlatAppearance.BorderColor = Color.White;
+            }
+
+            ShowRandomWord();
+        }
+
+        private void ctarih_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
